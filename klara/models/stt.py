2b55cce306c7f0e7e6faaa45a3d8c578519e9966
0x7o <danil.kononyuk@ya.ru>
2024-04-03 19:22:07 +0800

    Server

--- klara/models/stt.py
+++ klara/models/stt.py
@@ -1,23 +1,16 @@
-from transformers import WhisperProcessor, WhisperForConditionalGeneration
-import numpy as np
+import requests
 
 
 class STT:
-    def __init__(self, model_name="openai/whisper-base", sample_rate=16000):
-        self.processor = WhisperProcessor.from_pretrained(model_name)
-        self.model = WhisperForConditionalGeneration.from_pretrained(model_name)
-        self.model.config.forced_decoder_ids = None
-        self.sample_rate = sample_rate
+    def __init__(self, server_url="http://192.168.0.103:8000"):
+        self.server_url = server_url
 
     def process(self, audio):
-        audio_floats = np.concatenate(
-            [np.frombuffer(a, dtype=np.float32) for a in audio]
-        )
-        input_features = self.processor(
-            audio_floats, sampling_rate=self.sample_rate, return_tensors="pt"
-        ).input_features
-        predicted_ids = self.model.generate(input_features, language="ru")
-        transcription = self.processor.batch_decode(
-            predicted_ids, skip_special_tokens=True
-        )
-        return transcription[0].strip()
+        files = {"file": audio}
+        response = requests.post(f"{self.server_url}/transcribe", files=files)
+
+        if response.status_code == 200:
+            transcription = response.json()["text"]
+            return transcription
+        else:
+            raise Exception(f"Error {response.status_code}: {response.text}")
