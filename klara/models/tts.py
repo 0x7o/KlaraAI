788e8b60c2c7d361ef1d7da3107ebdadee1d13f2
0x7o <danil.kononyuk@ya.ru>
2024-04-03 17:39:10 +0800

    Server

--- klara/models/tts.py
+++ klara/models/tts.py
@@ -11,7 +11,7 @@ class TTS:
     def play_audio(self, audio_bytes):
         stream = self.p.open(format=pyaudio.paFloat32,
                              channels=1,
-                             rate=22050,
+                             rate=44100,
                              output=True)
         audio_array = np.frombuffer(audio_bytes, dtype=np.float32)
         stream.write(audio_array.tobytes())
