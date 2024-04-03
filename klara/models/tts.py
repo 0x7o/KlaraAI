d24c7746730ec2e9dc61c289f11fe8e6f8c5365f
0x7o <danil.kononyuk@ya.ru>
2024-04-03 18:33:10 +0800

    Server

--- klara/models/tts.py
+++ klara/models/tts.py
@@ -1,8 +1,5 @@
-import io
-import wave
 import aiohttp
 import asyncio
-import numpy as np
 import tempfile
 import os
 
@@ -11,37 +8,31 @@ class TTS:
     def __init__(self):
         self.endpoint = "http://192.168.0.103:5002/api/tts"
 
-    async def play_audio(self, audio_bytes):
-        with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
-            temp_file.write(audio_bytes)
-            temp_file_path = temp_file.name
-
-        command = f"aplay -D plughw:3,0 {temp_file_path}"
+    async def play_audio(self, file_name):
+        command = f"aplay -D plughw:3,0 {file_name}"
         process = await asyncio.create_subprocess_shell(command)
         await process.wait()
 
-        os.unlink(temp_file_path)
+        os.unlink(file_name)
 
     async def get_audio(self, text):
         async with aiohttp.ClientSession() as session:
             async with session.get(f"{self.endpoint}?text={text}") as response:
-                return await response.read()
+                bytes = await response.read()
+                with tempfile.NamedTemporaryFile(delete=False, suffix=".wav") as temp_file:
+                    temp_file.write(bytes)
+                    return temp_file.name
 
     async def say_async(self, text_list):
-        audio_bytes = await self.get_audio(text_list[0])
-        play_task = asyncio.create_task(self.play_audio(audio_bytes))
+        file_name = await self.get_audio(text_list[0])
+        play_task = asyncio.create_task(self.play_audio(file_name))
 
         for text in text_list[1:]:
-            audio_bytes = await self.get_audio(text)
+            file_name = await self.get_audio(text)
             await play_task
-            play_task = asyncio.create_task(self.play_audio(audio_bytes))
+            play_task = asyncio.create_task(self.play_audio(file_name))
 
         await play_task
 
     def say(self, text_list):
         asyncio.run(self.say_async(text_list))
-
-if __name__ == "__main__":
-    texts = ["И смех, и грех лопашыващывапвыал" for i in range(10)]
-    tts = TTS()
-    tts.say(texts)
