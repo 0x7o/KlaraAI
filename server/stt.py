9bef144f65e7fe43051d945f2ebf109e3d63c620
0x7o <danil.kononyuk@ya.ru>
2024-04-03 19:54:39 +0800

    Server

--- server/stt.py
+++ server/stt.py
@@ -8,19 +8,18 @@ import io
 app = FastAPI()
 
 device = "cuda:1" if torch.cuda.is_available() else "cpu"
-torch_dtype = torch.float16 if torch.cuda.is_available() else torch.float32
 
 model_id = "distil-whisper/distil-large-v3"
 
-model = AutoModelForSpeechSeq2Seq.from_pretrained(
-    model_id, torch_dtype=torch_dtype, use_safetensors=True
-)
+model = AutoModelForSpeechSeq2Seq.from_pretrained(model_id, use_safetensors=True)
 model.to(device)
 
 processor = AutoProcessor.from_pretrained(model_id)
 
+
 class TranscriptionResponse(BaseModel):
     text: str
+
 
 @app.post("/transcribe", response_model=TranscriptionResponse)
 async def transcribe(file: UploadFile = File(...)):
@@ -28,7 +27,9 @@ async def transcribe(file: UploadFile = File(...)):
     audio_stream = io.BytesIO(audio_bytes)
     sampling_rate, audio_data = wavfile.read(audio_stream)
 
-    input_features = processor(audio_data, sampling_rate=sampling_rate, return_tensors="pt").input_features
+    input_features = processor(
+        audio_data, sampling_rate=sampling_rate, return_tensors="pt"
+    ).input_features
     input_features = input_features.to(device)
 
     predicted_ids = model.generate(input_features, language="ru")
